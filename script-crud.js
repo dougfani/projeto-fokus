96b6038b77f42ed12d98029c466a12e1ce564167
Douglas Floriano <douglas97floriano@gmail.com>
2026-05-03 18:35:32 -0300

    Adiciona insercao de tarefa na lista ao clicar. Adiciona arquivos de imagem. Adiciona remocao do formulario ao salvar

--- script-crud.js
+++ script-crud.js
@@ -13,20 +13,21 @@ function criarElementoTarefa(tarefa) {
 
     const svg = document.createElement('svg');
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-        </svg>
         </svg>
     `;
 
     const paragrafo = document.createElement('p');
     paragrafo.textContent = tarefa.descricao;
+    paragrafo.classList.add('app__section-task-list-item-description');
 
     const botao = document.createElement('button');
+    botao.classList.add('app_button-edit');
+
     const imagemBotao = document.createElement('img');
-    imagemBotao.setAttribute('src,', '/imagens/edit.png');
+    imagemBotao.setAttribute('src', '/imagens/edit.png');
     botao.append(imagemBotao);
 
     li.append(svg);
@@ -46,7 +47,11 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
         descricao: textArea.value,
     };
     tarefas.push(tarefa);
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    textArea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
 });
 
 tarefas.forEach((tarefa) => {
