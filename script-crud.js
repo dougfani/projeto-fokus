29980d2b384f507b8eef2dfd2e18e84610835043
Douglas Floriano <douglas97floriano@gmail.com>
2026-05-04 19:29:34 -0300

    adiciona finalizacao da tarefa apos tempo de foco terminar

--- script-crud.js
+++ script-crud.js
@@ -9,6 +9,7 @@ const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-ta
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarTarefas() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -57,9 +58,11 @@ function criarElementoTarefa(tarefa) {
         if (tarefaSelecionada == tarefa) {
             paragrafoDescricaoTarefa.textContent = '';
             tarefaSelecionada = null;
+            liTarefaSelecionada = null;
             return;
         }
         tarefaSelecionada = tarefa;
+        liTarefaSelecionada = li;
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
 
         li.classList.add('app__section-task-list-item-active');
@@ -95,3 +98,11 @@ function limparFormulario() {
     textArea.value = '';
     formAdicionarTarefa.classList.toggle('hidden');
 }
+
+document.addEventListener('FocoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled');
+    }
+});
